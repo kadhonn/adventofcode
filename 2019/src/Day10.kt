7fc982ecb6f1c5a5ea0d27d2c41c3c1f5a05d5d6
Matthias Ableidinger <Matthias.Ableidinger@catalysts.cc>
2020-01-25 14:24:57 +0100

    day 10 part 1

---
+++ 2019/src/Day10.kt
@@ -0,0 +1,70 @@
+import kotlin.math.abs
+
+fun main() {
+    Day10.run()
+}
+
+object Day10 {
+    fun run() {
+        val input = String(ClassLoader.getSystemResourceAsStream("in2")!!.readAllBytes())
+
+        val field = input.split("\n")
+            .map { it.toCharArray() }
+            .map { it.map { it == '#' } }
+            .map { it.toList() }
+            .toList()
+
+        var max = 0
+        for (i in field.indices) {
+            for (j in field[i].indices) {
+                if (field[i][j]) {
+                    max = Math.max(max, calc(field, i, j))
+                }
+            }
+        }
+
+        println(max)
+    }
+
+    private fun calc(field: List<List<Boolean>>, x: Int, y: Int): Int {
+        val excluded = field.indices.flatMap { i -> field[i].indices.map { j -> Pair(i, j) } }
+            .filter { field[it.first][it.second] }
+            .filter { it.first != x || it.second != y }
+            .sortedBy { abs(it.first - x) + abs(it.second - y) }
+            .fold(mutableSetOf<Pair<Int, Int>>(), { excluded, nearestPoint ->
+                if (!excluded.contains(nearestPoint)) {
+                    val (dx, dy) = shrink(nearestPoint.first - x, nearestPoint.second - y)
+                    var movingX = nearestPoint.first + dx
+                    var movingY = nearestPoint.second + dy
+                    while (movingX >= 0 && movingX < field.size && movingY >= 0 && movingY < field[movingX].size) {
+                        if (field[movingX][movingY]) {
+                            excluded.add(Pair(movingX, movingY))
+                        }
+                        movingX += dx
+                        movingY += dy
+                    }
+                }
+                excluded
+            })
+
+        val totalAsteroids = field.flatMap { it }.filter { it }.count()
+        return totalAsteroids - excluded.size - 1
+    }
+
+    private val primes = listOf(2, 3, 5, 7, 11, 13, 17, 19, 21)
+
+    private fun shrink(initDx: Int, initDy: Int): Pair<Int, Int> {
+        var dx = initDx
+        var dy = initDy
+        var primeIndex = 0
+        while (primes[primeIndex] <= abs(dx) || primes[primeIndex] <= abs(dy)) {
+            if (dx % primes[primeIndex] == 0 && dy % primes[primeIndex] == 0) {
+                dx /= primes[primeIndex]
+                dy /= primes[primeIndex]
+            } else {
+                primeIndex++
+            }
+        }
+        return Pair(dx, dy)
+    }
+}
