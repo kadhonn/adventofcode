781bb9aebd80e40b4470777a053676bc51807f82
Matthias Ableidinger <ableimat@gmx.at>
2020-12-15 19:07:16 +0100

    day 15

---
+++ 2020/src/Day15_2.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day15_2 {
+    public static void main(String[] args) throws IOException {
+        String input = Files.readString(new File("./in/day15.in").toPath());
+
+        String[] startingNumbers = input.split(",");
+
+        Map<Integer, Integer> lastSeenInTurn = new HashMap<>();
+        int turn = 1;
+        int nextNumber = 0;
+
+        for (String startingNumber : startingNumbers) {
+            int number = Integer.parseInt(startingNumber);
+            nextNumber = getNextNumber(lastSeenInTurn, number, turn);
+            lastSeenInTurn.put(number, turn);
+            turn++;
+        }
+
+        while (turn < 30000000) {
+            int number = nextNumber;
+            nextNumber = getNextNumber(lastSeenInTurn, number, turn);
+            lastSeenInTurn.put(number, turn);
+            turn++;
+        }
+
+        System.out.println(nextNumber);
+    }
+
+    private static int getNextNumber(Map<Integer, Integer> lastSeenInTurn, int number, int turn) {
+        if (lastSeenInTurn.containsKey(number)) {
+            return turn - lastSeenInTurn.get(number);
+        } else {
+            return 0;
+        }
+    }
+
+}
