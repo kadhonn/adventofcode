1cb319c33c325d10238762f93a5ca5cbee00dba5
Matthias Ableidinger <ableimat@gmx.at>
2021-12-02 19:01:12 +0100

    day 2 part 2

--- 2021/aoc/src/main/kotlin/Day2.kt
+++ 2021/aoc/src/main/kotlin/Day2.kt
@@ -1,10 +1,10 @@
 fun main() {
-    Day2.doRun(ClassLoader.getSystemResource("day2_full.in").readText())
-//    Day2.doRun(ClassLoader.getSystemResource("day2_example.in").readText())
+    Day2.part2(ClassLoader.getSystemResource("day2_full.in").readText())
+//    Day2.part2(ClassLoader.getSystemResource("day2_example.in").readText())
 }
 
 object Day2 {
-    fun doRun(input: String) {
+    fun part1(input: String) {
         var (position, depth) = input.trim().split("\r\n")
             .map { it.split(" ") }
             .map { Pair(it[0], it[1].toInt()) }
@@ -13,6 +13,22 @@ object Day2 {
                     "forward" -> Pair(position.first + movement.second, position.second)
                     "up" -> Pair(position.first, position.second - movement.second)
                     "down" -> Pair(position.first, position.second + movement.second)
+                    else -> throw RuntimeException("wat")
+                }
+            }
+
+        println(position * depth)
+    }
+
+    fun part2(input: String) {
+        var (position, depth) = input.trim().split("\r\n")
+            .map { it.split(" ") }
+            .map { Pair(it[0], it[1].toInt()) }
+            .fold(Triple(0, 0, 0)) { position, movement ->
+                when (movement.first) {
+                    "forward" -> Triple(position.first + movement.second, position.second + movement.second * position.third, position.third)
+                    "up" -> Triple(position.first, position.second, position.third - movement.second)
+                    "down" -> Triple(position.first, position.second, position.third + movement.second)
                     else -> throw RuntimeException("wat")
                 }
             }
