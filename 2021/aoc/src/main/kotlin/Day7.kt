521a7cbab13b493a0eb6700e479785b721f73409
Matthias Ableidinger <ableimat@gmx.at>
2021-12-07 19:06:29 +0100

    day 7 part 1

---
+++ 2021/aoc/src/main/kotlin/Day7.kt
@@ -0,0 +1,25 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    Day7.part1(ClassLoader.getSystemResource("day7_full.in").readText().trim())
+//    Day7.part1(ClassLoader.getSystemResource("day7_example.in").readText().trim())
+}
+
+object Day7 {
+
+    fun part1(input: String) {
+        val crabs = input.split(",")
+            .map { it.toInt() }
+
+        val min = (crabs.minOrNull()!!..crabs.maxOrNull()!!).map { cost(crabs, it) }.minOrNull()
+
+        println(min)
+
+    }
+
+    private fun cost(crabs: List<Int>, middle: Int): Int {
+        return crabs.map { (it - middle).absoluteValue }.sum()
+    }
+
+}
+
