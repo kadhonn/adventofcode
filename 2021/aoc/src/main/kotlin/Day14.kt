e050954d0216050dae002f3ea910822bf52439b0
Matthias Ableidinger <ableimat@gmx.at>
2021-12-14 19:35:14 +0100

    day 14 part 2

--- 2021/aoc/src/main/kotlin/Day14.kt
+++ 2021/aoc/src/main/kotlin/Day14.kt
@@ -1,16 +1,21 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
-    Day14.part1(ClassLoader.getSystemResource("day14_full.in").readText().trim())
-//    Day14.part1(ClassLoader.getSystemResource("day14_example.in").readText().trim())
+    println(measureTimeMillis {
+        Day14.part1(ClassLoader.getSystemResource("day14_full.in").readText().trim())
+        //    Day14.part1(ClassLoader.getSystemResource("day14_example.in").readText().trim())
+    })
 }
 
 object Day14 {
 
-    val counts = mutableMapOf<Char, Int>()
     val mappings = mutableMapOf<String, Char>()
+    val cache = mutableMapOf<Triple<Int, Char, Char>, Map<Char, Long>>()
     fun part1(input: String) {
         val lines = input.split("\r\n")
         val template = lines[0]
 
+        var counts = mutableMapOf<Char, Long>()
         lines.drop(2).forEach {
             val splitted = it.trim().split(" -> ")
             mappings.put(splitted[0], splitted[1][0])
@@ -20,7 +25,8 @@ object Day14 {
         template.chars().forEach { counts[it.toChar()] = counts[it.toChar()]!! + 1 }
 
         for (i in 0..template.length - 2) {
-            recursiveCount(template[i], template[i + 1], 0)
+            val newCounts = recursiveCount(template[i], template[i + 1], 0)
+            counts = mergeMap(counts, newCounts)
         }
 
         val max = counts.values.maxOrNull()!!
@@ -28,17 +34,35 @@ object Day14 {
         println(max - min)
     }
 
-    val MAX_DEPTH = 10
-    private fun recursiveCount(first: Char, second: Char, depth: Int) {
+    val MAX_DEPTH = 40
+    private fun recursiveCount(first: Char, second: Char, depth: Int): Map<Char, Long> {
         if (depth == MAX_DEPTH) {
-            return
+            return emptyMap()
+        }
+        val triple = Triple(depth, first, second)
+        if (cache.containsKey(triple)) {
+            return cache[triple]!!
         }
         val newElement = mappings[first.toString() + second.toString()]!!
-        counts[newElement] = counts[newElement]!! + 1
 
-        recursiveCount(first, newElement, depth + 1)
-        recursiveCount(newElement, second, depth + 1)
+        val firstCount = recursiveCount(first, newElement, depth + 1)
+        val secondCount = recursiveCount(newElement, second, depth + 1)
+
+        val mergeMap = mergeMap(mergeMap(firstCount, secondCount), mapOf(Pair(newElement, 1)))
+        cache.put(triple, mergeMap)
+        return mergeMap
     }
 
+    private fun mergeMap(counts: Map<Char, Long>, newCounts: Map<Char, Long>): MutableMap<Char, Long> {
+        val map = mutableMapOf<Char, Long>()
+
+        map.putAll(counts)
+
+        newCounts.forEach {
+            map.merge(it.key, it.value, Long::plus)
+        }
+
+        return map
+    }
 }
 
