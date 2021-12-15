bc59c3959ac5a5a534d0056e027b4454d5a9cda4
Matthias Ableidinger <ableimat@gmx.at>
2021-12-15 19:22:55 +0100

    day 15 part 1

---
+++ 2021/aoc/src/main/kotlin/Day15.kt
@@ -0,0 +1,56 @@
+fun main() {
+    Day15.part1(ClassLoader.getSystemResource("day15_full.in").readText().trim())
+//    Day15.part1(ClassLoader.getSystemResource("day15_example.in").readText().trim())
+}
+
+object Day15 {
+
+    fun part1(input: String) {
+        val field = input.split("\r\n").map {
+            it
+                .trim()
+                .toCharArray()
+                .map { it.toString().toInt() }
+                .toTypedArray()
+        }.toTypedArray()
+
+        val end = Pair(field.size - 1, field[0].size - 1)
+
+        val reachedPoints = mutableSetOf<Pair<Int, Int>>()
+        val reachablePoints = mutableListOf<Triple<Int, Int, Int>>()
+
+        reachablePoints.add(Triple(0, 0, 0))
+
+        while (reachablePoints.isNotEmpty()) {
+            val min = reachablePoints.minByOrNull { it.third }!!
+            reachablePoints.remove(min)
+            val coords = Pair(min.first, min.second)
+            if (reachedPoints.contains(coords)) {
+                continue
+            }
+            reachedPoints.add(coords)
+
+            if (coords == end) {
+                println(min.third)
+                return
+            }
+
+            val nextPossiblePoints = listOf(
+                Pair(coords.first + 1, coords.second),
+                Pair(coords.first - 1, coords.second),
+                Pair(coords.first, coords.second + 1),
+                Pair(coords.first, coords.second - 1)
+            )
+
+            nextPossiblePoints.filter {
+                it.first >= 0 && it.first < field.size && it.second >= 0 && it.second < field[0].size
+            }.forEach {
+                reachablePoints.add(Triple(it.first, it.second, min.third + field[it.first][it.second]))
+            }
+        }
+
+        println("oh no")
+    }
+
+}
+
