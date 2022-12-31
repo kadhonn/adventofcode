c2aa376b0fd160ae835ba44bd6286b8279eae79e
abl <ableimat@gmx.at>
2022-12-31 22:25:38 +0100

    day 20 part 1

---
+++ 2022/src/day20.rs
@@ -0,0 +1,80 @@
+pub fn day20_1(str: &String) {
+    let numbers: Vec<i32> = str.trim().lines().map(|l| { l.parse().unwrap() }).collect();
+    let mut dll = make_dll(numbers.len());
+
+    // print_something(&numbers, &dll);
+    for i in 0..numbers.len() {
+        let steps = numbers[i];
+        if steps == 0 {
+            continue;
+        }
+        let mut current = i;
+        {
+            let current_pointers = dll[i];
+            dll[current_pointers.0].1 = current_pointers.1;
+            dll[current_pointers.1].0 = current_pointers.0;
+        }
+        if steps > 0 {
+            for _ in 0..steps {
+                current = dll[current].1;
+            }
+        } else {
+            for _ in steps..=0 {
+                current = dll[current].0;
+            }
+        }
+        let next = dll[current].1;
+        dll[current].1 = i;
+        dll[next].0 = i;
+        dll[i].0 = current;
+        dll[i].1 = next;
+
+        // print_something(&numbers, &dll);
+    }
+
+    let mut zero_i = 0;
+    for i in 0..numbers.len() {
+        if numbers[i] == 0 {
+            zero_i = i;
+            break;
+        }
+    }
+
+    let mut sum = 0;
+    let mut current = zero_i;
+    for i in 1..=3000 {
+        current = dll[current].1;
+        if i % 1000 == 0 {
+            println!("{}", numbers[current]);
+            sum += numbers[current];
+        }
+    }
+    println!("{}", sum);
+}
+
+fn print_something(numbers: &Vec<i32>, dll: &Vec<(usize, usize)>) {
+    let mut i = 0;
+    let start = dll[dll[0].0];
+    let mut current = start;
+    print!("{} -> ", numbers[current.1]);
+    current = dll[current.1];
+    while start != current {
+        print!("{} -> ", numbers[current.1]);
+        current = dll[current.1];
+        i += 1;
+        if i > numbers.len() {
+            panic!("loop detected");
+        }
+    }
+    println!();
+}
+
+fn make_dll(size: usize) -> Vec<(usize, usize)> {
+    let mut dll = vec![];
+
+    for i in 0..size {
+        dll.push(((i + size - 1) % size, (i + 1) % size));
+    }
+
+    dll
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code)]
 
+extern crate core;
+
 use std::fs;
 
-mod day19;
+mod day20;
 
 fn main() {
-    // let str = fs::read_to_string("in/in19_ex.txt").unwrap();
-    let str = fs::read_to_string("in/in19.txt").unwrap();
-    day19::day19_2(&str);
+    // let str = fs::read_to_string("in/in20_ex.txt").unwrap();
+    let str = fs::read_to_string("in/in20.txt").unwrap();
+    day20::day20_1(&str);
 }
