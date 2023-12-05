65d85d9b1af1b2151dff5080918ee52bf13a353b
abl <ableimat@gmx.at>
2023-12-05 23:56:32 +0100

    2023 day 5 part 2... BUT TOO SLOW

--- 2023/src/day5.rs
+++ 2023/src/day5.rs
@@ -41,3 +41,47 @@ pub fn day5_1(str: &str) {
 
     println!("{}", min);
 }
+
+pub fn day5_2(str: &str) {
+    let mut mapping_list: Vec<Vec<(i64, i64, i64, i64)>> = vec![];
+    let mut seeds: Vec<i64> = vec![];
+
+    for line in str.lines() {
+        if seeds.is_empty() {
+            for number in (&line[7..line.len()]).split(" ") {
+                seeds.push(number.trim().parse::<i64>().unwrap());
+            }
+            continue;
+        }
+        if line.is_empty() {
+            mapping_list.push(vec![]);
+            continue;
+        }
+        if !line.chars().collect::<Vec<char>>()[0].is_digit(10) {
+            continue;
+        }
+        let numbers = line.split(" ").map(|number| number.trim().parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        mapping_list.last_mut().unwrap().push((numbers[1], numbers[1] + numbers[2], numbers[0], numbers[0] + numbers[2]));
+    }
+
+    for i in 0..i64::MAX {
+        let mut current = i;
+        for mapping in mapping_list.iter().rev() {
+            'inner: for mapping_entry in mapping {
+                if mapping_entry.2 <= current && current <= mapping_entry.3 {
+                    current = current - mapping_entry.2 + mapping_entry.0;
+                    break 'inner;
+                }
+            }
+        }
+
+        for seed_i in 0..(seeds.len() / 2) {
+            let start = seeds[seed_i * 2];
+            let range = seeds[seed_i * 2 + 1];
+            if start <= current && current < start + range {
+                println!("{i}");
+                return;
+            }
+        }
+    }
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -9,5 +9,5 @@ mod day5;
 fn main() {
     // let str = fs::read_to_string("in/in5_ex.txt").unwrap();
     let str = fs::read_to_string("in/in5.txt").unwrap();
-    day5::day5_1(&str.trim());
+    day5::day5_2(&str.trim());
 }
