63c364f718349da2c6400f28b627fc1b94024ca9
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2023-12-05 14:59:42 +0100

    2023 day 3 part 2

--- 2023/src/day3.rs
+++ 2023/src/day3.rs
@@ -1,4 +1,5 @@
 use std::cmp::{max, min};
+use std::collections::HashMap;
 
 pub fn day3_1(str: &str) {
     let mut sum = 0;
@@ -35,5 +36,65 @@ pub fn day3_1(str: &str) {
         }
     }
 
+    println!("{}", sum);
+}
+
+pub fn day3_2(str: &str) {
+    let mut map: HashMap<(i32, i32), Vec<i32>> = HashMap::new();
+    let field = str.lines().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
+
+    for y in 0..field.len() {
+        let mut number = -1;
+        let mut coords_of_symbol = vec![];
+        for x in 0..field[y].len() {
+            let char = field[y][x];
+            if char.is_digit(10) {
+                let digit = char.to_digit(10).unwrap() as i32;
+                if number == -1 {
+                    number = digit;
+                } else {
+                    number = number * 10 + digit;
+                }
+                for i in max(0, y as i32 - 1)..=min(field.len() as i32 - 1, y as i32 + 1) {
+                    for j in max(0, x as i32 - 1)..=min(field[i as usize].len() as i32 - 1, x as i32 + 1) {
+                        let char = field[i as usize][j as usize];
+                        if char == '*' {
+                            let number = (i, j);
+                            if !coords_of_symbol.contains(&number) {
+                                coords_of_symbol.push(number);
+                            }
+                        }
+                    }
+                }
+            } else {
+                for coord in &coords_of_symbol {
+                    if map.contains_key(&coord) {
+                        map.get_mut(&(coord.clone())).unwrap().push(number);
+                    } else {
+                        let list = vec![number];
+                        map.insert(coord.clone(), list);
+                    }
+                }
+                number = -1;
+                coords_of_symbol = vec![];
+            }
+        }
+        for coord in &coords_of_symbol {
+            if map.contains_key(&coord) {
+                map.get_mut(&(coord.clone())).unwrap().push(number);
+            } else {
+                let list = vec![number];
+                map.insert(coord.clone(), list);
+            }
+        }
+    }
+
+    println!("{:?}", &map);
+    let mut sum = 0;
+    for entry in map {
+        if entry.1.len() == 2 {
+            sum += entry.1[0] * entry.1[1]
+        }
+    }
     println!("{}", sum);
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -9,5 +9,5 @@ mod day3;
 fn main() {
     // let str = fs::read_to_string("in/in3_ex.txt").unwrap();
     let str = fs::read_to_string("in/in3.txt").unwrap();
-    day3::day3_1(&str.trim());
+    day3::day3_2(&str.trim());
 }
