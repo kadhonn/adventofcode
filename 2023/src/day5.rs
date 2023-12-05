976487a346d510da413e39e86f72ab39e54ae9e6
abl <ableimat@gmx.at>
2023-12-06 00:28:38 +0100

    2023 day 5 part 2... MUCH FAST

--- 2023/src/day5.rs
+++ 2023/src/day5.rs
@@ -43,7 +43,7 @@ pub fn day5_1(str: &str) {
 }
 
 pub fn day5_2(str: &str) {
-    let mut mapping_list: Vec<Vec<(i64, i64, i64, i64)>> = vec![];
+    let mut mapping_list: Vec<Vec<(i64, i64, i64)>> = vec![];
     let mut seeds: Vec<i64> = vec![];
 
     for line in str.lines() {
@@ -61,27 +61,51 @@ pub fn day5_2(str: &str) {
             continue;
         }
         let numbers = line.split(" ").map(|number| number.trim().parse::<i64>().unwrap()).collect::<Vec<i64>>();
-        mapping_list.last_mut().unwrap().push((numbers[1], numbers[1] + numbers[2], numbers[0], numbers[0] + numbers[2]));
+        mapping_list.last_mut().unwrap().push((numbers[1], numbers[1] + numbers[2], numbers[0]));
     }
 
-    for i in 0..i64::MAX {
-        let mut current = i;
-        for mapping in mapping_list.iter().rev() {
-            'inner: for mapping_entry in mapping {
-                if mapping_entry.2 <= current && current <= mapping_entry.3 {
-                    current = current - mapping_entry.2 + mapping_entry.0;
-                    break 'inner;
+    for mapping in mapping_list.iter_mut() {
+        mapping.sort_by_key(|m| m.0);
+    }
+
+    let mut min = i64::MAX;
+    for seed_i in 0..(seeds.len() / 2) {
+        let start = seeds[seed_i * 2];
+        let end = start + seeds[seed_i * 2 + 1];
+        min = i64::min(min, find_min_for_range(&mapping_list, 0, start, end));
+    }
+
+    println!("{}", min);
+}
+
+fn find_min_for_range(mapping_list: &Vec<Vec<(i64, i64, i64)>>, mapping_i: usize, start: i64, end: i64) -> i64 {
+    if mapping_i == mapping_list.len() {
+        return start;
+    }
+    let mut min = i64::MAX;
+    let mut current_start = start;
+    let mapping = &mapping_list[mapping_i];
+    for mapping_entry in mapping {
+        if mapping_entry.0 > current_start {
+            let block_end = i64::min(mapping_entry.0, end);
+            min = i64::min(min, find_min_for_range(&mapping_list, mapping_i + 1, current_start, block_end));
+            current_start = block_end;
+            if current_start == end {
+                break;
+            }
+        } else {
+            if current_start < mapping_entry.1 {
+                let block_end = i64::min(mapping_entry.1, end);
+                min = i64::min(min, find_min_for_range(&mapping_list, mapping_i + 1, current_start - mapping_entry.0 + mapping_entry.2, block_end - mapping_entry.0 + mapping_entry.2));
+                current_start = block_end;
+                if current_start == end {
+                    break;
                 }
             }
         }
-
-        for seed_i in 0..(seeds.len() / 2) {
-            let start = seeds[seed_i * 2];
-            let range = seeds[seed_i * 2 + 1];
-            if start <= current && current < start + range {
-                println!("{i}");
-                return;
-            }
-        }
     }
+    if current_start != end {
+        min = i64::min(min, find_min_for_range(&mapping_list, mapping_i + 1, current_start, end));
+    }
+    return min;
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -3,11 +3,14 @@
 extern crate core;
 
 use std::fs;
+use std::time::Instant;
 
 mod day5;
 
 fn main() {
+    let before = Instant::now();
     // let str = fs::read_to_string("in/in5_ex.txt").unwrap();
     let str = fs::read_to_string("in/in5.txt").unwrap();
     day5::day5_2(&str.trim());
+    println!("took {:?}", before.elapsed());
 }
