d4bf01f8892383c20140b4caf5779f88456e78b7
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2023-12-06 10:11:42 +0100

    2023 day 6 part 2

--- 2023/src/day6.rs
+++ 2023/src/day6.rs
@@ -36,3 +36,33 @@ pub fn day6_1(str: &str) {
 
     println!("{}", prod);
 }
+
+pub fn day6_2(str: &str) {
+
+    let lines = str.lines().collect::<Vec<&str>>();
+
+    let mut times = String::new();
+    let mut max_distances = String::new();
+
+    let times_split=lines[0].split(" ").filter(|n| !n.trim().is_empty()).collect::<Vec<&str>>();
+    let distances_split=lines[1].split(" ").filter(|n| !n.trim().is_empty()).collect::<Vec<&str>>();
+
+    for time in 1..times_split.len() {
+        times.push_str(times_split[time].trim());
+    }
+    for distance in 1..distances_split.len() {
+        max_distances.push_str(distances_split[distance].trim());
+    }
+
+    let mut count = 0;
+    let time = times.parse::<i64>().unwrap();
+    let to_beat = max_distances.parse::<i64>().unwrap();
+    for chargeup in 1..time {
+        let result = (time - chargeup) * chargeup;
+        if result > to_beat {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
+}
