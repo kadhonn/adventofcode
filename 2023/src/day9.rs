566ae2d630a6f605e8abd3f2a0c81aabad68ac9a
abl <ableimat@gmx.at>
2023-12-10 12:21:35 +0100

    2023 day 9 part 1

---
+++ 2023/src/day9.rs
@@ -0,0 +1,44 @@
+use std::cmp::{max, min, Ordering};
+use std::collections::HashMap;
+use std::i64::MAX;
+use std::ops::{Index, Shl};
+use num_bigint::{BigInt, Sign};
+use regex::Regex;
+
+pub fn day9(str: &str) {
+    let mut sum = 0;
+
+    for line in str.lines() {
+        let mut sequences = vec![];
+        let numbers = line.split(" ").filter(|str| !str.trim().is_empty()).map(|str| str.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        sequences.push(numbers);
+
+        while true {
+            let mut found_non_zero = false;
+            let mut new_numbers = vec![];
+            let last_numbers = sequences.last().unwrap();
+            for i in 0..(last_numbers.len() - 1) {
+                let new_number = last_numbers[i + 1] - last_numbers[i];
+                new_numbers.push(new_number);
+                if new_number != 0 {
+                    found_non_zero = true;
+                }
+            }
+            if !found_non_zero {
+                break;
+            }
+            sequences.push(new_numbers);
+        }
+
+        let mut change = 0;
+        for i in (0..sequences.len()).rev() {
+            let last_numbers = &sequences[i];
+            let last_number = last_numbers[last_numbers.len() - 1];
+            change += last_number;
+        }
+
+        sum += change;
+    }
+
+    println!("{}", sum);
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -5,12 +5,12 @@ extern crate core;
 use std::fs;
 use std::time::Instant;
 
-mod day8;
+mod day9;
 
 fn main() {
     let before = Instant::now();
-    // let str = fs::read_to_string("in/in8_ex.txt").unwrap();
-    let str = fs::read_to_string("in/in8.txt").unwrap();
-    day8::day8(&str.trim());
+    // let str = fs::read_to_string("in/in9_ex.txt").unwrap();
+    let str = fs::read_to_string("in/in9.txt").unwrap();
+    day9::day9(&str.trim());
     println!("took {:?}", before.elapsed());
 }
