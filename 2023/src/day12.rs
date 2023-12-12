c689b3c1bbe5ecd325e57bd5e3e7c87a62069d33
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2023-12-12 10:55:26 +0100

    2023 day 12 part 2 :)

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -10,16 +10,30 @@ pub fn day12(str: &str) {
 
     for line in str.lines() {
         let split = line.split(" ").collect::<Vec<&str>>();
-        let chars = split[0].chars().collect::<Vec<char>>();
-        let damaged_groups = split[1].split(",").map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        let orig_chars = split[0].chars().collect::<Vec<char>>();
+        let orig_damaged_groups = split[1].split(",").map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>();
 
-        let mut valid_count = 0;
+        let mut chars = vec![];
+        let mut damaged_groups = vec![];
+        for _ in 0..5 {
+            for char in &orig_chars {
+                chars.push(char.clone());
+            }
+            chars.push('?');
+            for damaged_group in &orig_damaged_groups {
+                damaged_groups.push(damaged_group.clone());
+            }
+        }
+        chars.remove(chars.len() - 1);
+
         let mut stack = LinkedList::new();
+        let mut cache = HashMap::new();
 
-        println!();
-        println!();
-        println!("{}", line);
-        recursive_build(&chars, &damaged_groups, &mut stack, &mut valid_count);
+        // println!();
+        // println!();
+        // println!("{}", line);
+
+        let mut valid_count = recursive_build(&chars, &damaged_groups, &mut stack, &mut cache);
 
         println!("{valid_count}");
         sum += valid_count;
@@ -28,31 +42,41 @@ pub fn day12(str: &str) {
     println!("{sum}");
 }
 
-fn recursive_build(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &mut LinkedList<char>, valid_count: &mut i32) {
-    if !is_still_valid(chars, damaged_groups, stack) {
-        return;
-    }
-    if chars.len() == stack.len() {
-        *valid_count += 1;
-        println!("{}", stack.iter().collect::<String>());
-        return;
+fn recursive_build(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &mut LinkedList<char>, cache: &mut HashMap<(usize, i32), i64>) -> i64 {
+    let (valid, group_i) = is_still_valid(chars, damaged_groups, stack);
+    if !valid {
+        return 0;
     }
 
+    if chars.len() == stack.len() {
+        // println!("{}", stack.iter().collect::<String>());
+        return 1;
+    }
+
+    if let Some(entry) = cache.get(&(stack.len(), group_i)) {
+        return *entry;
+    }
+
+    let mut valid_count = 0;
     if chars[stack.len()] == '?' {
         stack.push_back('#');
-        recursive_build(chars, damaged_groups, stack, valid_count);
+        valid_count += recursive_build(chars, damaged_groups, stack, cache);
         stack.pop_back();
         stack.push_back('.');
-        recursive_build(chars, damaged_groups, stack, valid_count);
+        valid_count += recursive_build(chars, damaged_groups, stack, cache);
         stack.pop_back();
     } else {
         stack.push_back(chars[stack.len()]);
-        recursive_build(chars, damaged_groups, stack, valid_count);
+        valid_count += recursive_build(chars, damaged_groups, stack, cache);
         stack.pop_back();
     }
+    if group_i != -1 {
+        cache.insert((stack.len(), group_i), valid_count);
+    }
+    return valid_count;
 }
 
-fn is_still_valid(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &LinkedList<char>) -> bool {
+fn is_still_valid(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &LinkedList<char>) -> (bool, i32) {
     let solution = stack.iter().collect::<Vec<&char>>();
     let mut group_i = 0;
     let mut current_group_size = 0;
@@ -64,7 +88,7 @@ fn is_still_valid(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &LinkedLi
             continue;
         } else if !in_group && *char == '#' {
             if group_i == damaged_groups.len() {
-                return false;
+                return (false, -1);
             }
             current_group_size = 1;
             in_group = true;
@@ -72,7 +96,7 @@ fn is_still_valid(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &LinkedLi
             current_group_size += 1;
         } else if in_group && *char == '.' {
             if current_group_size != damaged_groups[group_i] {
-                return false;
+                return (false, -1);
             }
             group_i += 1;
             in_group = false;
@@ -82,9 +106,13 @@ fn is_still_valid(chars: &Vec<char>, damaged_groups: &Vec<i32>, stack: &LinkedLi
     }
 
     return if solution.len() == chars.len() {
-        (!in_group && group_i == damaged_groups.len()) ||
-            (in_group && group_i == damaged_groups.len() - 1 && current_group_size == damaged_groups[group_i])
+        ((!in_group && group_i == damaged_groups.len()) ||
+             (in_group && group_i == damaged_groups.len() - 1 && current_group_size == damaged_groups[group_i]), group_i as i32)
     } else {
-        true
+        if in_group {
+            (true, -1)
+        } else {
+            (true, group_i as i32)
+        }
     };
 }
