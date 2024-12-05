4572b48d377d9694c2ad32e63c5d6119547e8f79
Matthias Ableidinger <matthias.ableidinger@dynatrace.com>
2024-12-05 11:29:07 +0100

    move to cabal

---
+++ 2024/app/Main.hs
@@ -0,0 +1,6 @@
+import Day5_1
+
+main :: IO ()
+main = do
+    contents <- readFile "in.txt"
+    run contents
--- 2024/src/main.hs
+++
@@ -1,6 +0,0 @@
-import System.IO
-import Day4_2
-
-main = do
-    contents <- readFile "../in.txt"
-    run contents
