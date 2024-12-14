2a4c723114fc4bbc44d1d6de83aeb7a024d5e2fd
abl <ableimat@gmx.at>
2024-12-14 14:39:40 +0100

    day 12 part 2

--- 2024/app/Day12_1.hs
+++ 2024/app/Day12_1.hs
@@ -23,20 +23,77 @@ run input = do
       emptyOccupiedField = V.map (V.map (const False)) field
       newState = State 0 0 emptyOccupiedField []
       solvedState@(State _ _ _ regions) = solveState field newState
-      result = calcResult regions
+      result = calcResult field regions
   print field
   print newState
   print solvedState
   print result
 
-calcResult :: [Set.Set Point] -> Int
-calcResult = sum . map calcSingleResult
+calcResult :: Field Char -> [Set.Set Point] -> Int
+calcResult f = sum . map (calcSingleResult f)
 
-calcSingleResult :: Set.Set Point -> Int
-calcSingleResult region =
+calcSingleResult :: Field Char -> Set.Set Point -> Int
+calcSingleResult f region =
   let area = length region
-      perimeter = sum . map (length . filter (`notElem` region) . neighbours) . Set.toList $ region
-   in area * perimeter
+      borderFieldPairs = mconcat . map (\p -> map (p,) . filter (`notElem` region) . neighbours $ p) . Set.toList $ region
+      borders = map toBorder borderFieldPairs
+      borderMapInsert = M.insertWith (++)
+      borderMap = foldl' (\m (f, t) -> borderMapInsert f [t] (borderMapInsert t [f] m)) M.empty borders
+      (x, y) = head . Set.elems $ region
+      regionValue = f V.! y V.! x
+      sides = countSides f borderMap Set.empty regionValue
+   in area * trace (show borderMap) (trace (show sides) sides)
+
+countSides :: Field Char -> M.Map Point [Point] -> Set.Set Point -> Char -> Int
+countSides f m alreadyVisited rV =
+  let bordersLeft = filter (\(f, _) -> Set.notMember f alreadyVisited) . M.assocs $ m
+   in if null bordersLeft
+        then
+          0
+        else
+          let (cf, ct1 : _) = head . filter (\(k, [t1, t2]) -> isHorizontal k t1 /= isHorizontal k t2) $ bordersLeft
+              updatedSet = Set.insert cf (Set.insert ct1 alreadyVisited)
+              (resultSet, sides) = countSidesR f rV m cf updatedSet cf ct1
+           in sides + countSides f m resultSet rV
+
+countSidesR :: Field Char -> Char -> M.Map Point [Point] -> Point -> Set.Set Point -> Point -> Point -> (Set.Set Point, Int)
+countSidesR field rV m end alreadyVisited f t
+  | t == end = (alreadyVisited, 1)
+  | otherwise =
+      let next = getNext field rV m f t
+          isCorner = isHorizontal f t /= isHorizontal t next
+          count = if isCorner then 1 else 0
+          updatedSet = Set.insert next alreadyVisited
+          (resultSet, result) = countSidesR field rV m end updatedSet t next
+       in (resultSet, count + result)
+
+getNext field rV m f t =
+  let corner = filter (/= f) $ m M.! t
+   in if length corner == 1
+        then
+          head corner
+        else
+          let ((x1, y1), (x2, y2)) = toRegion (f, t)
+              correctPair = if field V.! y1 V.! x1 == rV then (x1, y1) else (x2, y2)
+           in head . filter (\cP -> let (p1, p2) = toRegion (t, cP) in correctPair == p1 || correctPair == p2) $ corner
+
+isHorizontal (x1, _) (x2, _) = x1 /= x2
+
+toBorder :: (Point, Point) -> (Point, Point)
+toBorder ((x1, y1), (x2, y2)) =
+  if x1 == x2
+    then
+      ((x1, max y1 y2), (x1 + 1, max y1 y2))
+    else
+      ((max x1 x2, y1), (max x1 x2, y1 + 1))
+
+toRegion :: (Point, Point) -> (Point, Point)
+toRegion ((x1, y1), (x2, y2)) =
+  if x1 == x2
+    then
+      ((x1, min y1 y2), (x1 - 1, min y1 y2))
+    else
+      ((min x1 x2, y1), (min x1 x2, y1 - 1))
 
 solveState :: Field Char -> State -> State
 solveState field s@(State x y alreadyOccupiedMap regions)
